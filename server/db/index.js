4414102b0384d4baaa2bc4072b5387f98f30a9d9
noorama <maxym.sydorov@rambler.ru>
2019-09-30 18:46:46 +0300

    database changes

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/testdb', { useNewUrlParser: true })
+    //    .connect('mongodb://127.0.0.1:27017/testdb', { useNewUrlParser: true })
+    .connect('mongodb://noorama:12345Wq@mongodb-y57t7.gcp.mongodb.net/testdb', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
