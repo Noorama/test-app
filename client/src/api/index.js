f2d3651717a8f34f67369ce2d541ecbbc8e8a7c5
noorama <maxym.sydorov@rambler.ru>
2019-09-30 19:31:41 +0300

    adress change to server listner for HEROKU

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-   // baseURL: 'http://localhost:3000/api',
-   baseURL: 'http://localhost:'+process.env.PORT+'/api',
+   baseURL: 'http://localhost:3000/api',
 })
 
 export const createComment = payload => api.post(`/comment`, payload)
